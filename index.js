d099bd3a66ba3129264d05990f1eb3f3477cc4fe
Steve Saldivar <steve.saldivar@outlook.com>
2023-10-19 15:40:12 -0700

    feat: add depth limit of 4
    npm: graphql-depth-limit

--- index.js
+++ index.js
@@ -2,7 +2,13 @@ const { ApolloServer } = require("apollo-server")
 const { typeDefs } = require('./schema/type-defs')
 const { resolvers } = require('./schema/resolvers')
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const depthLimit = require('graphql-depth-limit')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  validationRules: [depthLimit(4)]
+})
 
 server.listen().then(({ url }) => {
   console.log(`GraphQL is running on: ${url}`)
